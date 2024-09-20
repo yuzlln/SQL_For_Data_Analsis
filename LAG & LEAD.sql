bb9637883c9f63ad09caabafe66ef31e847ac6f2
Woo Yujin <biolet2001@gmail.com>
2024-09-20 17:18:25 +0900

    Update LAG & LEAD.sql

--- LAG & LEAD.sql
+++ LAG & LEAD.sql
@@ -1,10 +1,10 @@
 SELECT id
-	, date
-	, salary
+    , date
+    , salary
     
-	-- 1) 2칸 뒤에 있는 값이 null이면 0으로 대체
-	, LAG(salary, 2, 0) OVER(ORDER BY temperature) AS 'lag'
+    -- 1) 2칸 뒤에 있는 값이 null이면 0으로 대체
+    , LAG(salary, 2, 0) OVER(ORDER BY temperature) AS 'lag'
 	
-	-- 2) 2칸 앞에 있는 값이 null이면 0으로 대체
-	, LEAD(salary, 2, 0) OVER(ORDER BY temperature) AS 'lead'
+    -- 2) 2칸 앞에 있는 값이 null이면 0으로 대체
+    , LEAD(salary, 2, 0) OVER(ORDER BY temperature) AS 'lead'
 FROM sample;
